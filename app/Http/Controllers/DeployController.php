4b5da1bfd8d151faada0a949172ed7c96785cb38
FX-Max <812234333@qq.com>
2017-02-19 21:16:20 +0800

    add deploy webhook

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -12,7 +12,18 @@ class DeployController extends Controller
 
     public function deploy(){
 
-        
+        $target = '/var/www/http/wechat';
+        //$token = '';
+
+        //$json = json_decode(file_get_contents('php://input'), true);
+
+        //if (empty($json['token']) || $json['token'] !== $token) {
+        //    exit('error request');
+        //}
+
+        $cmd = "cd $target && git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master";
+
+        shell_exec($cmd);
 
     }
 
