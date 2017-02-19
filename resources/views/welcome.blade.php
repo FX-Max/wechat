fb2134245dfa083a114987c3e096c042a65a9453
FX-Max <812234333@qq.com>
2017-02-19 23:20:24 +0800

    change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello World Max!</div>
+                <div class="title">Hello World. I am Max!</div>
             </div>
         </div>
     </body>
