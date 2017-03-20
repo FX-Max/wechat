9c7feb1dc32fe370928cc8984d5177453df6eb04
FX-Max <812234333@qq.com>
2017-03-21 01:17:46 +0800

    start laravel command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    /**
+     * To start to use commands,add this in /etc/crontab
+     * * * * * * root /var/www/http/wechat/artisan schedule:run 1>>/dev/null 2>&1
+     */
+
     /**
      * The Artisan commands provided by your application.
      *
